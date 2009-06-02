019a5d9c256d6b9a38390fcb81914d602572b65b
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-06-02 17:33:57 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Author: Anand Patil
+# Date: 6 Feb 2009
+# License: Creative Commons BY-NC-SA
+####################################
+
+from setuptools import setup
+from numpy.distutils.misc_util import Configuration
+import os
+config = Configuration('anopheles',parent_package=None,top_path=None)
+
+config.packages = ["anopheles"]
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**(config.todict()))
