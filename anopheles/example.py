e23e84a0248fc9e96a1d1f3bbbd558f587b33a09
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-06-11 16:11:48 +0100

    Blah

--- anopheles/example.py
+++ anopheles/example.py
@@ -7,7 +7,7 @@ Get all sites where gambiae is found.
 Here we've filtered on name - could have just got all anophelines and looped over all 53, passing the anopheline object each time to query for sites.
 """
 session = Session()
-mozzie = session.query(Anopheline).filter(Anopheline.name.ilike("%albimanus%")).one()
+mozzie = session.query(Anopheline).filter(Anopheline.name.ilike("%bellator%")).one()
 
 species_specific_subquery = session.query(SamplePeriod.site_id,func.count('*').label('sample_period_count')).filter(SamplePeriod.anopheline==mozzie).group_by(SamplePeriod.site_id).subquery()
 any_anopheline = exists().where(SamplePeriod.site_id==Site.site_id)
@@ -15,6 +15,7 @@ any_anopheline = exists().where(SamplePeriod.site_id==Site.site_id)
 #SQL issued to db here - session queries are lazy.
 sites = session.query(Site.geom, species_specific_subquery.c.sample_period_count).outerjoin((species_specific_subquery, Site.site_id==species_specific_subquery.c.site_id)).filter(any_anopheline)
 mozzie_site_list = sites.all()
+
 
 #for eo in mozzie.expert_opinion:
 #    print eo.geom.centroid 
