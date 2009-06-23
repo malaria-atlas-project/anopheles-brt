be0d35a98af8eda3ebbfd80eea48dc6083e06ebb
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-06-23 18:53:38 +0100

    Oops

--- anopheles/testsuite/test_query.py
+++ anopheles/testsuite/test_query.py
@@ -3,10 +3,14 @@ import nose,  warnings
 from anopheles import species_query, list_species, Session
 
 class test_queries(object):
-    site_list, expert_opinion = species_query(Session(), 3)
-    for x in site_list:
-        if sum(count or 0 for count in x[1:4]) != x[4]:
-            print "Wrong."
+    def test_checksums(self):
+        "Tests that the entries of the records add up."
+        site_list, expert_opinion = species_query(Session(), 3)
+        for x in site_list:
+            assert(sum(count or 0 for count in x[1:4]) == x[4]):
+                
+    def some_other_test(self):
+        pass
 
 if __name__ == '__main__':
     nose.runmodule()
