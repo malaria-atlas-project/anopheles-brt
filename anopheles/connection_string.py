39ff8d34fa94a0b9889bf60272e999296e6ca566
Will Temperley <will@map.nsms.ox.ac.uk>
2009-07-29 10:17:25 +0100

    Not tracking connection string

--- anopheles/connection_string.py
+++
@@ -1,5 +0,0 @@
-from map import local_settings as ls
-
-#connection_string = 'postgres://anand:8lj23lafagjf@localhost:5432/map_vector3'
-#connection_string = 'postgres://will:\@&tolm00@localhost/map_vector3'
-connection_string = 'postgres://will:%s@%s:%s/%s' % (ls.DATABASE_PASSWORD, ls.DATABASE_HOST, ls.DATABASE_PORT, ls.DATABASE_NAME)
